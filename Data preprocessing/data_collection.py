c73312c8300ac4d11941431d444827566078eaa6
Mansour <mohammmmmmmmed852963@gmail.com>
2026-09-08 02:39:41 +0300

    made the final version of data collecting file and modified gitignore

--- Data preprocessing/data_collection.py
+++ Data preprocessing/data_collection.py
@@ -1,3 +1,10 @@
+import requests
+import os
+import json
+
+topic = "artificial intelligence OR machine learning OR generative ai OR openai OR anthropic OR nvidia OR chatgpt"
+# page_size = 20
+
 def create_document(id, source, title, content, date):
     return {
         "id": id,
@@ -7,45 +14,57 @@ def create_document(id, source, title, content, date):
         "date": date
     }
 
+def collect_news(topic=topic, page_size=20):
+    api_key = os.getenv("NEWS_API_KEY")
 
-def collect_sample_data():
+    url = "https://newsapi.org/v2/everything"
+
+    params = {
+        "q": topic,
+        "language": "en",
+        "sortBy": "publishedAt",
+        "pageSize": page_size
+    }
+
+    headers = {
+        "X-Api-Key": api_key
+    }
+
+    response = requests.get(url, params=params, headers=headers, timeout=10)
+    response.raise_for_status()
+
+    data = response.json()
+
+    articles = data["articles"]
     documents = []
 
-    documents.append(
-        create_document(
-            "news_001",
-            "newsapi",
-            "New AI Model Released",
-            "A new artificial intelligence model has been released. https://example.com/news",
-            "2026-09-07T12:00:00"
+    for i, article in enumerate(articles):
+        document = create_document(
+            id=f"news_{i+1:03d}",
+            source=article["source"]["name"],
+            title=article["title"],
+            content=article["content"],
+            date=article["publishedAt"]
         )
-    )
 
-    documents.append(
-        create_document(
-            "news_002",
-            "newsapi",
-            "New AI Model Released",
-            "A new artificial intelligence model has been released. https://example.com/news",
-            "2026-09-07T12:00:00"
-        )
-    )
-
-    documents.append(
-        create_document(
-            "reddit_001",
-            "reddit",
-            None,
-            "People are discussing the new AI model on Reddit.",
-            "2026-09-07T13:00:00"
-        )
-    )
+        documents.append(document)
 
     return documents
 
+def save_documents(documents, filename="Data preprocessing/Data/sample_data.json"):
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(documents, file, ensure_ascii=False, indent=4)
+
+def main():
+    documents = collect_news(topic)
+
+    print("Articles collected:", len(documents))
+
+    save_documents(documents)
+
+    print("Data saved successfully.")
+
+
 
 if __name__ == "__main__":
-    documents = collect_sample_data()
-
-    for document in documents:
-        print(document)
+    main()
